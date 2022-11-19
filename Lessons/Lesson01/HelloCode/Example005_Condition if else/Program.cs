709fa1addf204b7ba4960894f0aff7ffa54fb542
Sergey <sergeykozlenko@gmail.com>
2022-11-20 01:00:01 +0200

    Created 3-Code repo

---
+++ Lessons/Lesson01/HelloCode/Example005_Condition if else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Great - this is Maria!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
---
+++ Workshop/Task01/test/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести квадрат числа
+
+int val = 5;
+int res = val * val;
+Console.WriteLine(res);
+
+// end
