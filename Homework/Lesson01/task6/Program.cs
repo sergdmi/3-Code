fa134e49ee98d8d2d8698331ebba792291a0f3ea
Sergey <sergeykozlenko@gmail.com>
2022-11-21 14:40:54 +0200

    added Lesson01 task6

--- Homework/Lesson01/task6/Program.cs
+++ Homework/Lesson01/task6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напиcать программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter integral number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+double result = number % 2;
+
+if (result == 0) 
+{
+    Console.Write("even number");
+}
+
+else 
+{
+    Console.Write("odd number");
+}
+
+// end
