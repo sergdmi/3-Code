99c06c5680bb059b783987dd2f8b685eda26e48c
Sergey <sergeykozlenko@gmail.com>
2022-11-21 13:03:34 +0200

    added task 4

---
+++ Homework/Lesson01/task4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напиcать программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Enter number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) 
+{
+    max = b;
+}
+
+else
+{
+
+}
+
+if (c > max) 
+{
+    max = c;
+}
+
+else
+{
+    
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+// end
