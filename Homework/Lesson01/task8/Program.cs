7829fed138585b1eea94650c5a05f48bab415656
Sergey <sergeykozlenko@gmail.com>
2022-11-21 15:46:10 +0200

    added task 8 Lesson 01

---
+++ Homework/Lesson01/task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter integral number:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int number = 2;
+
+while (number <= N)
+{
+    Console.WriteLine(number);
+    number = number + 2;
+}
+
+// end
