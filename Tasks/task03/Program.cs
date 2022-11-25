48979deb930daad4ff602fa09c3d7c130401ebec
Sergey <sergeykozlenko@gmail.com>
2022-11-26 00:53:51 +0200

    added task03 to Tasks

---
+++ Tasks/task03/Program.cs
@@ -0,0 +1,16 @@
+﻿// По заданному номеру дня недели вывести его название
+
+// 1. Определить массив дней недели
+// 2. Ввести номер дня недели
+// 3. Вывести название дня недели c индексом номера
+
+// start
+
+string[] weekarray = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+Console.WriteLine("Enter day of the week number");
+int daynumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+Console.WriteLine(weekarray[daynumber]);
+
+// end
