51c2652e34234ed3c0e970933bba95432f4cc06f
Sergey <sergeykozlenko@gmail.com>
2022-11-29 00:54:13 +0200

    HW task38 Lesson05 added

---
+++ Homework/Lesson05/task38/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задать массив вещественных чисел. Найти разницу между максимальным и минимальным элементами массива.
+
+// Создать массив вещественнных чисел заданного размера (метод)
+// Заполнить массив из консоли (метод)
+// Вывести массив в консоль (метод)
+// Определить разницу между максимальным и минимальным элементом массива
+
+// start
+
+double[] CreateArray(int count)
+{
+  return new double[count];
+}
+
+void FillArrayConsole(double[] numbers)
+{
+     
+    for (int i=0; i < numbers.Length; i++)
+    {
+        Console.Write($" Введите элемент массива с индексом {i}: ");
+        numbers[i] = double.Parse(Console.ReadLine());
+    }
+   
+}
+
+string Print(double[] numbers)
+{
+    Console.WriteLine(); 
+    Console.WriteLine("Вывод массива:");
+
+    string text = "[";
+
+    for (int i=0; i < numbers.Length; i++)
+    {
+        text += ($"{numbers[i]} ");
+    }
+    return(text +"]");
+}
+
+double MaxMinNumDiff (double[] numbers)
+{
+    double minNumber = numbers[0];
+    double maxNumber = numbers[1]; 
+
+    if (numbers[1] < numbers[0])
+    {
+        minNumber = numbers[1];
+        maxNumber = numbers[0];      
+    }
+     
+
+    for(int i = 2; i < numbers.Length; i++)
+    {
+        if (numbers[i] < minNumber)
+        {
+            minNumber = numbers[i];
+        }
+
+        if (numbers[i] > maxNumber)
+        {
+            maxNumber = numbers[i];
+        }
+    }
+
+    double result =  maxNumber - minNumber;
+
+    return result;
+
+}
+
+double[] array = CreateArray(5);
+FillArrayConsole(array);
+Console.WriteLine(Print(array));
+Console.WriteLine(); 
+Console.WriteLine($"Разница между максимальным и минимальным значениями: {MaxMinNumDiff(array)}");
+
+// end
