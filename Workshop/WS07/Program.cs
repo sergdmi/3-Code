f06a6e797078c544518687a5d3d4e46ac23a0d54
Sergey <sergeykozlenko@gmail.com>
2022-11-30 00:02:07 +0200

    WS07 and WS07MAIN added

---
+++ Workshop/WS07/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+int[,] pascal = new int[11, 11];
+
+for (int n = 0; n < 11; n++)
+{
+    pascal[n, 0] = 1;
+    pascal[n, n] = 1;
+    
+}
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 11; j++)
+    {
+        Console.Write($"{pascal[i,j],4}");
+    }
+    Console.WriteLine();
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine();
+
+
+for (int i = 2; i < 11; i++)
+{
+    for (int j = 1; j < 11; j++)
+    {
+        pascal[i, j] = pascal[i - 1, j - 1] + pascal[i - 1, j];
+    }
+}
+
+for (int i = 0; i < 11; i++)
+{
+    for (int j = 0; j < 11; j++)
+    {
+       if(pascal[i,j]!=0) Console.Write($"{pascal[i,j],4}");
+    }
+    Console.WriteLine();
+}
+
+// pascal [i, j] = pascal [i-1, j-1] +  pascal [i-1, j];
---
+++ Workshop/WS07MAIN/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+
+void PrintTriangle(int[,] triangle)
+{
+  int row = triangle.GetLength(0);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < row; j++)
+    {
+      if (triangle[i, j] != 0)
+        Console.Write($"{triangle[i, j],4}");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[] GetKoeff(int[,] tr, int pow)
+{
+  int[] row = new int[pow + 1];
+  for (int i = 0; i <= pow; i++)
+  {
+    row[i] = tr[pow, i];
+  }
+  return row;
+}
+
+int[,] tr = CreateTriangle(10);
+PrintTriangle(tr);
+int[] koeff = GetKoeff(tr, 2);
+
+System.Console.WriteLine(String.Join(' ', koeff));
+
+System.Console.WriteLine(koeff.Length);
+
+for (int i = 0; i < koeff.Length; i++)
+{
+  Console.Write($"{koeff[i]}*a^{koeff.Length - 1 - i}*b^{i} + ");
+}
