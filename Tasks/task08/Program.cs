6e841afc6dd3aa86ca759a25f650b8a0239d8a2a
Sergey <sergeykozlenko@gmail.com>
2022-12-14 21:47:21 +0200

    task08 added to Tasks

---
+++ Tasks/task08/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать четные числа от 1 до N
+
+
+// start
+
+Console.WriteLine("Ввести целое число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int number = 1;
+
+while (number <= N)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    number = number + 1;
+}
+
+// end
