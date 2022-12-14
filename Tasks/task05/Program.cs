fa8c1b518cbbd7554174d9eece50d9229a7bd44d
Sergey <sergeykozlenko@gmail.com>
2022-12-14 17:55:34 +0200

    task05 added to Tasks

---
+++ Tasks/task05/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Написать программу вычисления значения функции y = f(a)
+
+// start
+
+double Func(double a)
+{
+    double y = a*a+1;
+
+    return y;
+}
+
+
+double res = Func(2.5);
+
+Console.WriteLine(res);
+
