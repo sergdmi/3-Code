1ef1908c8147ad2a0ab6235ffe6f92d83b47dd47
Sergey <sergeykozlenko@gmail.com>
2022-12-16 18:47:36 +0200

    task17 added to Tasks

---
+++ Tasks/task17/Program.cs
@@ -0,0 +1,23 @@
+﻿//  По двум заданным числам проверять является ли одно квадратом другого
+
+// start
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == y * y)
+{
+    Console.WriteLine("Число x является квадратом числа y");
+}
+else
+
+    Console.WriteLine("Число x не является квадратом числа y");
+
+
+// end
