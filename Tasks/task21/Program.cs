7f089dcbd8a4ffeb7fd51c2b12d44fcf47b3809b
Sergey <sergeykozlenko@gmail.com>
2022-12-17 00:58:19 +0200

    task21 added to Tasks

---
+++ Tasks/task21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа проверяет пятизначное число на палиндром.
+
+// start
+
+Console.Clear();
+
+Console.WriteLine("ввести пятизначное целое число: ");
+
+string value = Console.ReadLine();
+
+char[] array = new char[value.Length];
+
+for(int i = 0; i < value.Length; i++)
+{
+    array[i] = value[i];
+}
+
+if (array[0] == array [4] && array[1] == array[3])
+{
+    Console.Write("палиндром");
+}
+
+else Console.Write("не палиндром");
+
+// end
