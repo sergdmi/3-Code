2f9a267a6432dc07df69402af161ee9f729cde2e
Sergey <sergeykozlenko@gmail.com>
2022-12-16 12:08:53 +0200

    task09 added to Tasks

---
+++ Tasks/task09/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+// start
+
+Console.WriteLine("Введите 3-хзначное число:");
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+
+int c = value % 10;         
+
+Console.WriteLine(c);
+
+
+// end
