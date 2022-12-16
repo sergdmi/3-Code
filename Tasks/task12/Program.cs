6d9fcf68f52b03cfdd589fc057aa14f4db3e7cb2
Sergey <sergeykozlenko@gmail.com>
2022-12-16 15:29:22 +0200

    task12 added to Tasks

---
+++ Tasks/task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Удалить вторую цифру трёхзначного числа.
+
+// start
+
+Console.Clear();
+
+Console.WriteLine("start");
+
+int value = new Random().Next(100, 1000);  // [100; 999]
+
+int a = value / 100;
+int b = (value / 10) % 10;
+int c = value% 10;
+
+int result = a*10 + c;
+
+Console.WriteLine(value);
+Console.WriteLine(result);
+Console.WriteLine("end");
+
+// end
