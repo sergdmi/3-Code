5af457dc817a7bb83f2cab1e6a7ae7cd241c64bb
Sergey <sergeykozlenko@gmail.com>
2022-12-16 15:50:22 +0200

    task13 added to Tasks

---
+++ Tasks/task13/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+
+// start
+
+Console.Clear();
+Console.WriteLine("Ввести исходное целое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввести множитель: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a % b;
+
+if ( c == 0)
+{
+    Console.WriteLine ("кратно");
+}
+
+else 
+Console.Write($"Остаток: {c}");
+
+
+// end
