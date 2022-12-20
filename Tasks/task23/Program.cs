1c872c70b7d9f08a50b6fc87560806ee2be6a9e6
Sergey <sergeykozlenko@gmail.com>
2022-12-20 23:10:22 +0200

    task23 Tasks added another desicion

---
+++ Tasks/task23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+
+// start
+
+Console.Clear();
+
+Console.WriteLine("Ввести целое число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int n = 1;
+
+while (n <= N)
+{
+    Console.WriteLine($"{n}^2 = {n*n}");
+    n = n + 1;
+}
+
+// end
