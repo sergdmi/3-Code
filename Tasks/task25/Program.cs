1811ce2692e3f66463d60e76dff9e0ffd0cbcd26
Sergey <sergeykozlenko@gmail.com>
2022-12-27 23:53:48 +0200

    task25 Tasks added

---
+++ Tasks/task25/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Найти сумму чисел от 1 до А
+
+// start
+
+Console.Clear();
+
+Console.WriteLine("Ввести целое число");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int n = 1;
+
+while (n <= A)
+{
+    sum = sum + n;
+    n = n + 1;
+}
+
+Console.Write("сумма:");
+Console.WriteLine(sum);
+
+// end
