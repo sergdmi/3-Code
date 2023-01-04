5d6d862c2a77360ca38bdd0471229cd2604063c1
Sergey <sergeykozlenko@gmail.com>
2023-01-04 22:28:27 +0200

    task30 Tasks added

---
+++ Tasks/task30/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+// start
+
+Console.Clear();
+
+Console.WriteLine("Ввести целое число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int n = 1;
+
+while (n <= N)
+{
+    if (n % 2 == 0) {Console.WriteLine($"{n}^3 = {n*n*n}");}
+    n = n + 1;
+}
+
+// end
