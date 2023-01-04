be0cc84476f1b26e662055a8b5836900d6d85d92
Sergey <sergeykozlenko@gmail.com>
2023-01-04 21:44:27 +0200

    task29 Tasks added

---
+++ Tasks/task29/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+// start
+
+// Console.Clear();
+// Console.WriteLine("Ввести натуральное число N");
+
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int n = 1;
+// double mult = 1;
+
+// while (n <= N)
+// {
+//     mult = mult * n;
+//     n = n + 1;
+// }
+
+// Console.WriteLine($"произведение чисел от 1 до N: {mult}");
+
+// end
+
+// Решение через рекурсию
+
+// start
+
+Console.Clear();
+Console.WriteLine("Ввести натуральное число N");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+double FactorialNum(int num)
+{
+    if (num == 1) return 1;
+
+    else return FactorialNum(num - 1) * num;
+
+}
+
+Console.Write($"произведение чисел от 1 до N: {FactorialNum(N)}");
+
+// end
