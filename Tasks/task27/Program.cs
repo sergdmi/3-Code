d33be0f1083397e7764855b8b73c1e8f0ce42353
Sergey <sergeykozlenko@gmail.com>
2023-01-04 18:13:40 +0200

    task27 Tasks added

---
+++ Tasks/task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Определить количество цифр в числе
+
+// start
+
+Console.Clear();
+Console.WriteLine("Ввести натуральное число");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int n = Number;
+
+while (n >= 1)
+{
+    n = n / 10;
+    i = i + 1;
+}
+
+Console.WriteLine($"Количество цифр: {i}");
+
+// end
