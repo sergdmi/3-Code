786a17376fa4ce1f9bdb412a2716ec617b28bab5
Sergey K <sergeykozlenko@gmail.com>
2023-01-06 01:00:12 +0200

    Tasks added task34

---
+++ Tasks/task34/Program.cs
@@ -0,0 +1,49 @@
+﻿// Написать программу замены элементов массива на противоположные 
+
+// start
+
+Console.Clear();
+
+int[] CreateArray(int count)
+{
+    return new int[count];
+}
+
+void FillArrayRandom(int[] randomNumber)
+{
+    for (int i = 0; i < randomNumber.Length; i++)
+    {
+        randomNumber[i] = new Random().Next(-100, 100);
+    }
+}
+
+string Print(int[] numbers)
+{
+    Console.WriteLine(); 
+    
+    string text = "[ ";
+
+    for (int i=0; i < numbers.Length; i++)
+    {
+        text += ($"{numbers[i]} ");
+    }
+    return(text + "]");
+}
+
+void RevertArray(int[] initarray)
+{
+    int size = initarray.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        initarray[i] = - initarray[i];
+    }
+}
+
+int[] array1 = CreateArray(10);
+FillArrayRandom(array1);
+Console.WriteLine(Print(array1));
+RevertArray(array1);
+Console.WriteLine(Print(array1));
+
+// end
