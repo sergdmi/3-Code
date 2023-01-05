df9f3ab69faccca012c56a1b0226a2dfd5e95d83
Sergey K <sergeykozlenko@gmail.com>
2023-01-06 01:09:08 +0200

    Tasks added task32

---
+++ Tasks/task32/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+// start
+
+Console.Clear();
+
+int[] numbers = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 2);
+}
+
+Console.Write("[ ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
+Console.Write("]");
+
+// end
