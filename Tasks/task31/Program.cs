6dead5add2a3aad26f15f3355bf0f2e6eecf61c5
Sergey K <sergeykozlenko@gmail.com>
2023-01-05 21:42:11 +0200

    Tasks added task31

---
+++ Tasks/task31/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать массив из 8 элементов и вывести их на экран 
+
+// start
+
+Console.Clear();
+
+int[] numbers = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($" Введите элемент массива с индексом {i}: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine();
+Console.WriteLine("Вывод массива:");
+
+Console.Write("[ ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]}, ");
+}
+Console.Write("]");
+
+// end
